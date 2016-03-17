83c5c156aa9c1d214bb0507703ef4e53b7e63d0b
Alex Bertram <alex@bedatadriven.com>
2016-03-17 21:12:22 +0100

    Added benchmarks from AT&T 25 for testing

---
+++ att/README.md
@@ -0,0 +1,7 @@
+
+# AT&T Benchmarks 
+
+These benchmarks are based on the 
+["AT&T" benchmarks](http://r.research.att.com/benchmarks/R-benchmark-25.R)
+
+They have been adapted for Renjin, and broken into standalone scripts.
---
+++ att/matrix1/matrix1.R
@@ -0,0 +1,8 @@
+a <- matrix(rnorm(2500*2500)/10, ncol=2500, nrow=2500);
+b <- t(a);
+dim(b) <- c(1250, 5000);
+a <- t(b)
+
+# Ensure that the result is actually used to ensure
+# that Renjin doesn't optimize away the result
+print(sum(colMeans(a)))
