4e6129107c09a84df49395c1073360e557a0a8ce
Alex Bertram <alex@bedatadriven.com>
2016-07-15 11:44:23 +0200

    Fixed distance correlation benchmark

--- survey/dcor5k/dcor5k.R
+++ survey/dcor5k/dcor5k.R
@@ -12,18 +12,24 @@ load("GSS.2010.CS.rda")
 GSS.2010.CS.df <- GSS.2010.CS.df[ sample(1:nrow(GSS.2010.CS.df), size = 5000), ]
 
 nr <- nrow(GSS.2010.CS.df)
-vars <- names(GSS.2010.CS.df)
+vars <- attr(GSS.2010.CS.df, "var.labels")
+
+# clean up 
 
 # Find correlated pairs of variables
-# Skip the first 10 variables which are year, id, etc
-for(i in 10:nr)  {
-  for(j in (i+1):nr) {
+# Skip the first 2 variables which are year, id, etc
+# and limit to 400 variables otherwise the benchmark will
+# run too long.
+for(i in 3:400)  {
+  for(j in (i+1):400) {
     x <- GSS.2010.CS.df[, i]
     y <- GSS.2010.CS.df[, j]
     valid <- !(is.na(x) | is.na(y))
-    result <- DCOR(as.double(x[valid]), as.double(y[valid]))
-    if(result$dCor > 0.5) {
-      cat(sprintf("%.3f %15s x %15s\n", result$dCor, vars[i], vars[j]))
+    if(any(valid)) {
+        result <- DCOR(as.double(x[valid]), as.double(y[valid]))
+        if(result$dCor > 0.6) {
+          cat(sprintf("%.3f '%s' x '%s'\n", result$dCor, vars[i], vars[j]))
+        }
     }
   }
 }
