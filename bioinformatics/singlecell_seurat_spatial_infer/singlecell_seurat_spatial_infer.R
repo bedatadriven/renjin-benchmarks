7780ee691ef1bf08026d6df6b322549297ebe30d
Parham Solaimani <parham@bedatadriven.com>
2017-01-16 13:25:07 +0100

    code fix
    function names have changed in the new version of Seurat

--- bioinformatics/singlecell_seurat_spatial_infer/singlecell_seurat_spatial_infer.R
+++ bioinformatics/singlecell_seurat_spatial_infer/singlecell_seurat_spatial_infer.R
@@ -10,6 +10,9 @@ START_WORKFLOW <- as.numeric(Sys.time())
 # reproducibility
 set.seed(8008)
 ## packages
+library(useful)
+library(methods)
+library(utils)
 rVersion <- R.Version()
 if ('engine' %in% names(rVersion) && rVersion$engine == "Renjin") {
 	library(org.renjin.github.satijalab:Seurat)
@@ -35,48 +38,48 @@ zfish.log <- log(zfish.data + 1)
 # (expression >0.01), and all cells with > 2k genes. Cells will be initially
 # assigned to an identity class (grouping) based on the first field (underscore-delimited)
 zf.all <- new("seurat", raw.data = zfish.log)
-zf.all <- setup(zf.all, project = "zfish", min.cells = 3, min.genes = 2000, is.expr = 0.01, names.field = 1, names.delim = "_")
+zf.all <- Setup(zf.all, project = "zfish", min.cells = 3, min.genes = 2000, is.expr = 0.01, names.field = 1, names.delim = "_")
 
 zf.all
 
 ### Basic exploration of data
 
 # View the expression of genes and basic metrics across samples
-vlnPlot(zf.all, c("ACTB2", "VENT", "CHD", "nGene"))
+VlnPlot(zf.all, c("ACTB2", "VENT", "CHD", "nGene"))
 
 # Plot two cells against each other
 #Set do.ident=TRUE to identify outliers by clicking on points (ESC to exit after)
 
 par(mfrow = c(2,2))
-cellPlot(zf.all, zf.all@cell.names[1], zf.all@cell.names[2], do.ident = FALSE)
-cellPlot(zf.all ,zf.all@cell.names[3], zf.all@cell.names[4], do.ident = FALSE)
+CellPlot(zf.all, zf.all@cell.names[1], zf.all@cell.names[2], do.ident = FALSE)
+CellPlot(zf.all ,zf.all@cell.names[3], zf.all@cell.names[4], do.ident = FALSE)
 
 #Plot two genes against each other, can do this in limited groups of cells
-genePlot(zf.all, "MIXL1", "OSR1", cex.use = 1)
-genePlot(zf.all, "MIXL1", "SOX3", cell.ids = which.cells(zf.all, "zf1"), cex.use = 1)
+GenePlot(zf.all, "MIXL1", "OSR1", cex.use = 1)
+GenePlot(zf.all, "MIXL1", "SOX3", cell.ids = WhichCells(zf.all, "zf1"), cex.use = 1)
 
 ###Identify variable genes across the single cells
 #Genes placed into 20 bins based on X-axis (average expression). Y-axis is within-bin z-score of log(Variance/mean).
-zf.all <- mean.var.plot(zf.all, y.cutoff = 2, do.plot = TRUE, x.low.cutoff = 0.25, x.high.cutoff = 7, fxn.x = expMean, fxn.y = logVarDivMean)
+zf.all <- MeanVarPlot(zf.all, y.cutoff = 2, do.plot = TRUE, x.low.cutoff = 0.25, x.high.cutoff = 7, fxn.x = expMean, fxn.y = logVarDivMean)
 
 #remove markers which primarily define batch
-markers.remove <- batch.gene(zf.all, idents.use = c("zf1", "zf2", "zf3"), genes.use = zf.all@var.genes, auc.cutoff = 0.7)
-zf.all@var.genes <- zf.all@var.genes[!(zf.all@var.genes%in%markers.remove)]
+markers.remove <- BatchGene(zf.all, idents.use = c("zf1", "zf2", "zf3"), genes.use = zf.all@var.genes, auc.cutoff = 0.7)
+zf.all@var.genes <- zf.all@var.genes[!(zf.all@var.genes %in% markers.remove)]
 length(zf.all@var.genes)
 
 ###Run PCA, examine results
 #To demonstrate, run PCA on all genes (note that the result isn't great, PC1/2 contain little known biology)
-zf.all <- pca(zf.all, pc.genes = rownames(zf.all@data), pcs.print = 3, genes.print = 5)
+zf.all <- PCA(zf.all, pc.genes = rownames(zf.all@data), pcs.print = 3, genes.print = 5)
 
 #Instead, run PCA on zf.all@var.genes (default value for pc.genes).
-zf.all <- pca(zf.all, pcs.print = 3, genes.print = 5)
-pca.plot(zf.all, pt.size = 2)
+zf.all <- PCA(zf.all, pcs.print = 3, genes.print = 5)
+PCAPlot(zf.all, pt.size = 2)
 
 #If desired, project PCA structure across entire dataset (so all genes are included)
-zf.all <- project.pca(zf.all, do.print = FALSE)
+zf.all <- ProjectPCA(zf.all, do.print = FALSE)
 #visualize top genes for each PC, use.full selects the projected PCA. See also print.pca
 #Explicit thanks to Olga Botvinnik, who showed me this visualization in her Flotilla package
-viz.pca(zf.all, pcs.use = 1:3, num.genes = 10, use.full = TRUE, nCol = 3)
+VizPCA(zf.all, pcs.use = 1:3, num.genes = 10, use.full = TRUE, nCol = 3)
 
 ###Create new object without EVL cells
 #Identify EVL cells (see Manuscript for further detail)
@@ -89,32 +92,32 @@ names(evl.quant) <- colnames(zf.all@data)
 not.evl <- names(evl.quant[evl.quant > 0])
 is.evl <- names(evl.quant[evl.quant < 0])
 
-points(zf.all@pca.rot[is.evl, 1], zf.all@pca.rot[is.evl, 2],pch = 16,col = "red")
+points(zf.all@pca.rot[is.evl, 1], zf.all@pca.rot[is.evl, 2], pch = 16, col = "red")
 
 #subsetData allows you to create a new Seurat object with a subset of the data
-zf <- subsetData(zf.all,cells.use = not.evl)
+zf <- SubsetData(zf.all, cells.use = not.evl)
 
 ###Identify genes to use for building gene expression models
 #recalculate a set of variable genes, now that EVL are removed
-zf <- mean.var.plot(zf, y.cutoff = 2, do.plot = FALSE, x.low.cutoff = 1, x.high.cutoff = 7, fxn.x = expMean, fxn.y = logVarDivMean, set.var.genes = TRUE)
-markers.remove <- batch.gene(zf, idents.use = c("zf1", "zf2", "zf3"),genes.use = zf@var.genes)
-zf@var.genes <- zf@var.genes[!(zf@var.genes%in%markers.remove)]
+zf <- MeanVarPlot(zf, y.cutoff = 2, do.plot = FALSE, x.low.cutoff = 1, x.high.cutoff = 7, fxn.x = expMean, fxn.y = logVarDivMean, set.var.genes = TRUE)
+markers.remove <- BatchGene(zf, idents.use = c("zf1", "zf2", "zf3"), genes.use = zf@var.genes)
+zf@var.genes <- zf@var.genes[!(zf@var.genes %in% markers.remove)]
 
 #redo the PCA on the variable genes.
-zf <- pca(zf, do.print = FALSE)
+zf <- PCA(zf, do.print = FALSE)
 
 #Run a 'random' PCA 1,000 times - scrambling a random 2.5% of the data each time
 #This enables us to identify statistically significant PCs (in this case, 1:3), and genes with significant PC scores
-zf <- jackStraw(zf, num.replicate = 1000, prop.freq = 0.025)
-jackStrawPlot(zf)
+zf <- JackStraw(zf, num.replicate = 1000, prop.freq = 0.025)
+JackStrawPlot(zf)
 
-zf <- project.pca(zf, do.print = FALSE,do.center = FALSE)
-genes.sig <- pca.sig.genes(zf, pcs.use = c(1, 2, 3), pval.cut = 1e-2, use.full = TRUE)
+zf <- ProjectPCA(zf, do.print = FALSE, do.center = FALSE)
+genes.sig <- PCASigGenes(zf, pcs.use = c(1, 2, 3), pval.cut = 1e-2, use.full = TRUE)
 
-plot.1 <- pca.plot(zf, do.return = TRUE)
-plot.2 <- pca.plot(zf, 1, 3, do.return = TRUE)
-multiplotList(list(plot.1, plot.2), cols = 2)
-viz.pca(zf, pcs.use = 1:3, num.genes = 10, nCol = 3)
+plot.1 <- PCAPlot(zf, do.return = TRUE)
+plot.2 <- PCAPlot(zf, 1, 3, do.return = TRUE)
+MultiPlotList(list(plot.1, plot.2), cols = 2)
+VizPCA(zf, pcs.use = 1:3, num.genes = 10, nCol = 3)
 
 ### Build models of gene expression
 # Matrices of gene expression were generated from published in situ stainings, and saved in an Excel file (which eases data entry). So, we import this data and add it to the Seurat object.
@@ -132,46 +135,46 @@ zf@insitu.matrix <- insitu.matrix[ , insitu.genes]
 lasso.genes.use <- unique(c(genes.sig,zf@var.genes))
 
 # we will fit models for the landmark genes using the 'structured' genes (with significant PCA scores), and variable genes
-zf <- addImputedScore(zf, genes.use = lasso.genes.use, genes.fit = insitu.genes, do.print = FALSE, s.use = 40, gram = FALSE)
+#zf <- AddImputedScore(zf, genes.use = lasso.genes.use, genes.fit = insitu.genes, do.print = FALSE, s.use = 40, gram = FALSE)
 
 # Demonstrate the benefit of imputation
 #before imputation - MIXL1 and OSR1 should be tightly co-expressed (on t he left)
 par(mfrow = c(1, 2))
-genePlot(zf, "MIXL1", "OSR1", col = "black", cex.use = 1)
+GenePlot(zf, "MIXL1", "OSR1", col = "black", cex.use = 1)
 #after imputation (on the right)
-genePlot(zf, "MIXL1", "OSR1", use.imputed = TRUE, col = "black", cex.use = 1)
+#GenePlot(zf, "MIXL1", "OSR1", use.imputed = TRUE, col = "black", cex.use = 1)
 
 
 ### Build Mixture models of Gene Expression
 
 # The in situ patterns that we use to provide geographical information are scored in a binary on/off format. In order to translate the continuous RNAseq data into this form, we model it as mixtures of 2 normal distributions that represent the on state and off state. We then use this to estimate whether each cell should be considered on or off for each gene.
 insitu.genes <- colnames(zf@insitu.matrix)
-for(i in rev(insitu.genes)) zf <- fit.gene.k(zf, i, do.plot = FALSE, do.k = 2, start.pct = mean(zf@insitu.matrix[ , i]), num.iter = 1)
+for(i in rev(insitu.genes)) zf <- FitGeneK(zf, i, do.plot = FALSE, do.k = 2, start.pct = mean(zf@insitu.matrix[ , i]), num.iter = 1)
 
 #show an example mixture model
 par(mfrow = c(2, 2))
-zf_temp <- fit.gene.k(zf, "SOX3", do.plot = TRUE, do.k = 2, start.pct = mean(zf@insitu.matrix[ , "SOX3"]))
-zf_temp <- fit.gene.k(zf, "OSR1", do.plot = TRUE, do.k = 2, start.pct = mean(zf@insitu.matrix[ , "OSR1"]))
-zf_temp <- fit.gene.k(zf, "BAMBIA", do.plot = TRUE, do.k = 2, start.pct = mean(zf@insitu.matrix[ , "BAMBIA"]))
-zf_temp <- fit.gene.k(zf, "SEBOX", do.plot = TRUE, do.k = 2, start.pct = mean(zf@insitu.matrix[ , "SEBOX"]))
+zf_temp <- FitGeneK(zf, "SOX3", do.plot = TRUE, do.k = 2, start.pct = mean(zf@insitu.matrix[ , "SOX3"]))
+zf_temp <- FitGeneK(zf, "OSR1", do.plot = TRUE, do.k = 2, start.pct = mean(zf@insitu.matrix[ , "OSR1"]))
+zf_temp <- FitGeneK(zf, "BAMBIA", do.plot = TRUE, do.k = 2, start.pct = mean(zf@insitu.matrix[ , "BAMBIA"]))
+zf_temp <- FitGeneK(zf, "SEBOX", do.plot = TRUE, do.k = 2, start.pct = mean(zf@insitu.matrix[ , "SEBOX"]))
 
 
 ### Project each cell into its proper location
 #Perform an initial mapping of cells to bins
-zf <- initial.mapping(zf)
+zf <- InitialMapping(zf)
 
 #Now, perform the quantitative refinement procedure (see manuscript for details)
 
 #first identify the genes to use for the refinement (top 3 genes in both directions for PC1-3)
-genes.use <- pcTopGenes(zf, pc.use = 1:3, num.genes = 6, use.full = TRUE, do.balanced = TRUE)
+genes.use <- PCTopGenes(zf, pc.use = 1:3, num.genes = 6, use.full = TRUE, do.balanced = TRUE)
 
 #impute values for these genes if needed
 new.imputed <- genes.use[!genes.use %in% rownames(zf@imputed)]
-lasso.genes.use <- unique(c(zf@var.genes, pca.sig.genes(zf, pcs.use = c(1, 2, 3), pval.cut = 1e-2, use.full = TRUE, )))
-zf <- addImputedScore(zf, genes.use = lasso.genes.use, genes.fit = new.imputed, do.print = FALSE, s.use = 40, gram = FALSE)
+lasso.genes.use <- unique(c(zf@var.genes, PCASigGenes(zf, pcs.use = c(1, 2, 3), pval.cut = 1e-2, use.full = TRUE, )))
+zf <- AddImputedScore(zf, genes.use = lasso.genes.use, genes.fit = new.imputed, do.print = FALSE, s.use = 40, gram = FALSE)
 
 #refine the mapping with quantitative models that also consider gene covariance
-zf <- refined.mapping(zf, genes.use)
+zf <- RefinedMapping(zf, genes.use)
 
 
 ### Brief analysis of the mapped cells
@@ -179,7 +182,7 @@ zf <- refined.mapping(zf, genes.use)
 corner(zf@final.prob)
 
 #calculate centroids for each cell
-zf.centroids <- get.centroids(zf)
+zf.centroids <- GetCentroids(zf)
 colnames(zf.centroids) <- c("bin.tier", "bin.dv")
 corner(zf.centroids)
 
@@ -190,10 +193,10 @@ zf <- addMetaData(zf, zf.centroids)
 #We can use genePlot to examine relationships between different types of variables
 #Note that individual genes (or PCs) display strong, but noisy and non-linear, relationships with Seurat's mapping positions along both axes
 par(mfrow = c(2, 2))
-genePlot(zf, "OSR1", "bin.tier", cex.use = 1)
-genePlot(zf, "BAMBIA", "bin.dv", cex.use = 1)
-genePlot(zf, "PC1", "bin.tier", cex.use = 1)
-genePlot(zf, "PC2", "bin.dv", cex.use = 1)
+GenePlot(zf, "OSR1", "bin.tier", cex.use = 1)
+GenePlot(zf, "BAMBIA", "bin.dv", cex.use = 1)
+GenePlot(zf, "PC1", "bin.tier", cex.use = 1)
+GenePlot(zf, "PC2", "bin.dv", cex.use = 1)
 
 
 ##### Beautiful Zebrafish visaulizations courtesy of Jeff Farrell (jfarrell@g.harvard.edu)
@@ -218,17 +221,17 @@ margin.cells <- rownames(subset(zf.centroids, bin.tier > 5))
 zf.margin <- subsetData(zf, cells.use = margin.cells)
 #note, could replace the last three lines with: zf.margin=subsetData(zf,subset.name = "bin.tier",accept.low = 5)
 
-zf.margin <- pca(zf.margin, do.print = FALSE)
-zf.margin <- jackStraw(zf.margin, num.replicate = 1000, prop.freq = 0.025)
-zf.margin <- project.pca(zf.margin, pcs.print = 3, genes.print = 8, do.center = FALSE)
+zf.margin <- PCA(zf.margin, do.print = FALSE)
+zf.margin <- JackStraw(zf.margin, num.replicate = 1000, prop.freq = 0.025)
+zf.margin <- ProjectPCA(zf.margin, pcs.print = 3, genes.print = 8, do.center = FALSE)
 
-kmeans.genes <- pca.sig.genes(zf.margin, pcs.use = 1:3, pval.cut = 1e-3)
+kmeans.genes <- PCASigGenes(zf.margin, pcs.use = 1:3, pval.cut = 1e-3)
 zf.margin <- doKMeans(zf.margin, genes.use = kmeans.genes, k.genes = 7, k.cells = 8, pc.row.order = 3, pc.col.order = 3, rev.pc.order = TRUE, cexRow = 0.5, cexCol = 0.5)
 #We can see high Gsc in cluster 5, and high Sox32 in cluster 1.
 #Note that clusters (cells and genes) are ordered by average PC3 score, just for visualization.
 
-endo.cells <- which.cells(zf.margin, 1)
-plate.cells <- which.cells(zf.margin, 5)
+endo.cells <- WhichCells(zf.margin, 1)
+plate.cells <- WhichCells(zf.margin, 5)
 zf.margin <- set.ident(zf.margin, zf.margin@cell.names, "All")
 zf.margin <- set.ident(zf.margin, endo.cells, "Endoderm")
 zf.margin <- set.ident(zf.margin, plate.cells, "PCP")
@@ -236,8 +239,8 @@ zf.margin <- set.ident(zf.margin, plate.cells, "PCP")
 pop.cols <- c("#999999","#1B75BB","#37B34A")
 
 #plot PCA, coloring cells by their status/ID (stored in zf@ident)
-pca.plot(zf.margin, 1, 2, pt.size = 3.5, cols.use = pop.cols)
-pc.13 <- pca.plot(zf.margin, 1, 3, pt.size = 3.5, cols.use = pop.cols)
+PCAPlot(zf.margin, 1, 2, pt.size = 3.5, cols.use = pop.cols)
+pc.13 <- PCAPlot(zf.margin, 1, 3, pt.size = 3.5, cols.use = pop.cols)
 
 #gene-gene plot, again, coloring cells by their stat/ID
 genePlot(zf.margin, "GSC", "FRZB", col.use = pop.cols)
@@ -258,7 +261,7 @@ subset(pcp.markers, avg_diff > 0 & p_val < 0.01)[1:15, ]
 subset(endoderm.markers, avg_diff > 0 & p_val < 0.01)[1:15, ]
 
 # Draw violin plots of known and new markers
-vlnPlot(zf.margin, c("GSC", "SOX32", "RIPPLY1", "FRZB"), cols.use = pop.cols, nCol = 2)
+VlnPlot(zf.margin, c("GSC", "SOX32", "RIPPLY1", "FRZB"), cols.use = pop.cols, nCol = 2)
 
 
 ###Localize cellular populati ons
