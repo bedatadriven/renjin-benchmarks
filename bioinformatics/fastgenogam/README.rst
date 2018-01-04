bc10ca3f39cb9ae9968cc60d7140543ab1a6e3c9
Parham Solaimani <parham@bedatadriven.com>
2018-01-04 15:41:54 +0100

    add fastgenogam and fastgenogam3 benchmarks

---
+++ bioinformatics/fastgenogam/README.rst
@@ -0,0 +1,49 @@
+
+fastgenogam
+===========
+
+a fast version of GenoGAM. Available here `on GitHub`_.
+
+[//]: # (.. figure:: ../../docs/_static/genogam.pdf)
+[//]: # (   :scale: 120 %)
+[//]: # (   :alt: Diagram affy)
+[//]: # (   )
+[//]: # (   Diagram of 'affy' analysis workflow.)
+
+
+Packages and Dependencies
+-------------------------
+
+There are 5 packages used in this workflow, which depend
+on . . . additional packages from CRAN and Bioconductor (dependencies)
+
+**Used packages:**
+
+* *Bioconductor*: SummarizedExperiment, HDF5Array, S4Vectors, Matrix
+
+* *CRAN*: 
+
+**Package dependencies:**
+
+* *Bioconductor*: Rsamtools, GenomicRanges, BiocParallel, data.table, DESeq2, futile.logger, GenomeInfoDb, GenomicAlignments, IRanges, Biostrings, DelayedArray, chipseq, genefilter
+
+* *CRAN*: . . .
+
+
+
+.. _on GitHub: https://github.com/gstricker/fastGenoGAM
+
+
+License
+-------
+
+* Copyright (c) 2017 GPL-2, based on code from https://github.com/gstricker/fastGenoGAM
+* Copyright (c) 2017-2018 BeDataDriven B.V.  License: `Apache License version 2.0 or higher`_
+
+.. _Apache License version 2.0 or higher: http://www.apache.org/licenses/LICENSE-2.0
+
+
+.. raw:: latex
+
+    \clearpage
+
---
+++ bioinformatics/fastgenogam3/fastgenogam3.R
@@ -0,0 +1,36 @@
+library(fastGenoGAM)
+
+## ================
+## General workflow
+## ================
+
+## data
+config <- "config_h3k4me3.txt"
+
+## specify number of cores
+BiocParallel::register(BiocParallel::SnowParam(worker = 3))
+
+## Read in data, the chunk and the overhang give the tile,
+## the units into which the entire genome will be cut for parallel computation
+## While chunks are disjoint, tiles are overlapping.
+chunkSize <- 20000 
+overhangSize <- 1000 
+
+## set custom HDF5 folder if needed, otherwise it will use the
+## default dump folder of HDF5Array
+settings <- GenoGAMSettings(hdf5Control = list(dir = "sacCer3_h5"))#,
+                            #chromosomeList = c("any chromosome")) ## restrict to only some chromosomes if needed, otherwise leave argument out.
+
+## with HDF5
+ggd <- GenoGAMDataSet(config, design = ~ s(x) + s(x, by = genotype), directory = folder, hdf5 = TRUE, chunkSize = chunkSize, settings = settings, overhangSize = overhangSize)
+
+## size factor
+ggd <- computeSizeFactors(ggd)
+
+## Run 
+lambda <- NULL ## set to NULL to use Cross Validation
+theta <- NULL ## set to NULL to use Cross Validation
+result <- genogam(ggd, intervalSize = 200, lambda = lambda, theta = theta)
+
+
+
